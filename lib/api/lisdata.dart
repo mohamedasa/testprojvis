c66272875b15425e79dce3cf04aa0a255dd26523
fox <m.asaad7193@gmail.com>
2026-02-16 22:07:19 +0200

    new add class lisdata

---
+++ lib/api/lisdata.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+
+class Lisdata extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("这是一个列表"),
+      );
+}
+}
